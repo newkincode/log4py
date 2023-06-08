cb84d2ad251fb9545cb4d0926bd83496a80e7e03
newkini-dev <94734506+newkincode@users.noreply.github.com>
2023-06-08 19:17:35 +0900

    Update log4py.py

--- log4py.py
+++ log4py.py
@@ -15,32 +15,32 @@ class Logger:
     def debug(self, text):
         self.now = datetime.datetime.now()
         self.log.append(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[36mDEBUG\033[0m {text}")
-        print(self.log[len(self.log)-1])
+            f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} DEBUG {text}")
+        print(f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[36mDEBUG\033[0m {text}")
 
     def info(self, text):
         self.now = datetime.datetime.now()
         self.log.append(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[94mINFO\033[0m \033[0m{text}")
-        print(self.log[len(self.log)-1])
+            f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} INFO {text}")
+        print(f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[94mINFO\033[0m \033[0m{text}")
 
     def warning(self, text):
         self.now = datetime.datetime.now()
         self.log.append(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[33mWARNING\033[0m \033[0m{text}")
-        print(self.log[len(self.log)-1])
+            f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} WARNING {text}")
+        print(f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[33mWARNING\033[0m \033[0m{text}")
 
     def error(self, text):
         self.now = datetime.datetime.now()
         self.log.append(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[91mERROR\033[0m \033[0m{text}")
-        print(self.log[len(self.log)-1])
+            f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} ERROR {text}")
+        print(f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[91mERROR\033[0m \033[0m{text}")
 
     def critical(self, text):
         self.now = datetime.datetime.now()
         self.log.append(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[31mCRITICAL\033[0m \033[0m{text}")
-        print(self.log[len(self.log)-1])
+            f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} CRITICAL {text}")
+        print(f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[31mCRITICAL\033[0m \033[0m{text}")
 
     def save(self):
         self.now = datetime.datetime.now()
