d090b1c9c0737cf86cb3f4f63b453dbf80e00137
newkincode <maainnewkin59@gmail.com>
2025-05-10 17:47:14 +0900

    log4py.

--- log4py.py
+++ log4py.py
@@ -6,6 +6,8 @@ class Logger:
     def __init__(self):
         self.now = datetime.datetime.now()
         self.log = []
+        self.log_print = True
+        self.save_log = True
 
     def custem(self, text):
         self.now = datetime.datetime.now()
@@ -15,37 +17,42 @@ class Logger:
     def debug(self, text):
         self.now = datetime.datetime.now()
         self.log.append(f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} DEBUG {text}")
-        print(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[36mDEBUG\033[0m {text}"
-        )
+        if self.log_print:
+            print(
+                f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[36mDEBUG\033[0m {text}"
+            )
 
     def info(self, text):
         self.now = datetime.datetime.now()
         self.log.append(f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} INFO {text}")
-        print(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[94mINFO\033[0m \033[0m{text}"
-        )
+        if self.log_print:
+            print(
+                f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[94mINFO\033[0m \033[0m{text}"
+            )
 
     def warning(self, text):
         self.now = datetime.datetime.now()
         self.log.append(f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} WARNING {text}")
-        print(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[33mWARNING\033[0m \033[0m{text}"
-        )
+        if self.log_print:
+            print(
+              f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[33mWARNING\033[0m \033[0m{text}"
+            )
 
     def error(self, text):
         self.now = datetime.datetime.now()
         self.log.append(f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} ERROR {text}")
-        print(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[91mERROR\033[0m \033[0m{text}"
-        )
+        if self.log_print:
+            print(
+                f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[91mERROR\033[0m \033[0m{text}"
+            )
 
     def critical(self, text):
         self.now = datetime.datetime.now()
         self.log.append(f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} CRITICAL {text}")
-        print(
-            f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[31mCRITICAL\033[0m \033[0m{text}"
-        )
+        if self.log_print:
+            print(
+                f"\033[95m{self.now.strftime('%Y-%m-%d %H:%M:%S')} \033[31mCRITICAL\033[0m \033[0m{text}"
+            )
 
     def save(self):
         self.now = datetime.datetime.now()
@@ -68,3 +75,7 @@ class Logger:
                 f.write(f"{i}\n")
             f.close()
         os.chdir(os.getcwd())
+
+    def __delattr__(self, name):
+        if self.save_log:
+            self.save()
